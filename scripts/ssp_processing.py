44d018fb2206b15a992f206d35f89a50053c894d
matthew-man-457 <mgjman457@gmail.com>
2022-03-15 20:52:21 +0100

    updated ssp_processing.py script

--- scripts/ssp_processing.py
+++ scripts/ssp_processing.py
@@ -1,37 +1,69 @@
-import time
-import os
-
 import math
 import numpy as np
-import pandas as pd
-import seaborn as sns
+##import pandas as pd
+##import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.fftpack import rfft, rfftfreq
 
 #from kafka import KafkaProducer
 
-from hdf5libs import *
+import sys
+
+from hdf5libs import HDF5RawDataFile
+import daqdataformats
+import detdataformats.ssp
+from rawdatautils import *
+import h5py
 
 
 # Parameters
 #filname = "/eos/experiment/neutplatform/protodune/rawdata/np04/vd-protodune-pds/raw/2021/detector/test/None/00/01/21/65/np02_pds_run012165_0000_20211126T181018.hdf5" 
-filename = "/eos/user/m/mman/np02_arapucas_run011966_0000_20211102T141910.hdf5"
-num_events = 1
-save_path = "/afs/cern.ch/user/m/mman/dune_daq/ssp_testing/test_output/"
+filename = "/eos/user/m/mman/np02_pds_run012450_0000_20220225T100020.hdf5.copied" #np02_arapucas_run011966_0000_20211102T141910.hdf5"
+#num_events = 1
+save_path = "/afs/cern.ch/user/m/mman/dunedaq-v2.9.0/ssp_test_out/"
 
-# Process file
-ssp_data = SSPDecoder(filename, num_events)
+# initialize variable lists
+module_id = []
+channel_id = []
+ext_timestamp = []
+peaksum = []
+peaktime = []
+prerise = []
+intsum = []
+baseline = []
+baseline_sum = []
 
+# Extract event_header info
+ssp_file = HDF5RawDataFile(filename)
+
+for dpath in ssp_file.get_all_fragment_dataset_paths():
+    frag = ssp_file.get_frag(dpath)
+    if frag.get_element_id().system_type!=daqdataformats.GeoID.SystemType.kPDS:
+        print("WARNING: fragment is not PDS type")
+        break
+
+    ## Parse SSP frag
+    event_header = detdataformats.ssp.EventHeader(frag.get_data())
+    ts = (event_header.timestamp[3] << 48) + (event_header.timestamp[2] << 32) + (event_header.timestamp[1] << 16) + (event_header.timestamp[0] << 0)
+    ext_timestamp.append(ts)
+    module_id.append((event_header.group2 & 0xFFF0) >> 4)
+    channel_id.append((event_header.group2 & 0x000F) >> 0)
+    peaktime.append((event_header.group3 & 0xFF00) >> 8)
+    prerise.append(((event_header.group4 & 0x00FF) << 16) + event_header.preriseLow)
+    intsum.append((event_header.intSumHigh << 8) + ((event_header.group4 & 0xFF00) >> 8))
+    baseline.append(event_header.baseline)
+    baseline_sum.append(((event_header.group4 & 0x00FF) << 16) + event_header.preriseLow)
+
+# Extract waveforms
+ssp_data = SSPDecoder(filename)
 ssp_frames = ssp_data.ssp_frames
 
-df = pd.DataFrame({"module_id": ssp_data.module_id,
-                   "channel_id": ssp_data.channel_id,
-                   "intsum": ssp_data.intsum})
+if (len(module_id) != len(ssp_frames)):
+    print("Fix Me!")
 
-fragment_count = len(ssp_data.module_id)
-
-##print(len(module_channel_id))
-##print(len(ssp_data.ssp_frames[0]))
+fragment_count = len(module_id)
+#print(fragment_count)
+n_frags_saved = 28
 
 ## Generate plots
 
@@ -39,8 +71,8 @@ fragment_count = len(ssp_data.module_id)
 fig = plt.figure(figsize=(35,15))
 ax = fig.add_subplot()
 
-for i in range(fragment_count):
-    plt.scatter(np.arange(len(ssp_frames[i])), ssp_frames[i], s=40, label="Module_Channel_ID "+str(ssp_data.module_id[i])+"_"+str(ssp_data.channel_id[i]))
+for i in range(n_frags_saved):
+    plt.scatter(np.arange(len(ssp_frames[i])), ssp_frames[i], s=40, label="Module_Channel_ID "+str(module_id[i])+"_"+str(channel_id[i]))
 
 plt.ylabel("ADC value")
 plt.xlabel('Time ticks')
@@ -56,14 +88,14 @@ plt.clf()
 fig = plt.figure(figsize=(35,15))
 ax = fig.add_subplot()
 
-for i in range(fragment_count):
+for i in range(n_frags_saved):
     ## sanity check
     ##if len(ssp_frames[i])<480:
     ##    print(ssp_frames[i])
     ##    continue
     fft = np.abs(rfft(ssp_frames[i]))
     freq = rfftfreq(482)
-    plt.plot(freq, fft, label="Module_Channel_ID "+str(ssp_data.module_id[i])+"_"+str(ssp_data.channel_id[i]))
+    plt.plot(freq, fft, label="Module_Channel_ID "+str(module_id[i])+"_"+str(channel_id[i]))
 
 plt.ylabel("FFT")
 plt.xlabel("Frequency")
@@ -72,17 +104,19 @@ plt.legend()
 plt.savefig(save_path+"FFT.png")
 
 ## Integrated sum ADC values
-fig = plt.figure(figsize=(35,15))
+#if num_events==1:
+#    fig = plt.figure(figsize=(35,15))
 
-pivot_data = df.pivot(index="module_id", columns="channel_id", values="intsum")
-ax = sns.heatmap(pivot_data)
+#    pivot_data = df.pivot(index="module_id", columns="channel_id", values="intsum")
+#    ax = sns.heatmap(pivot_data)
 
-plt.title("Integrated sum ADC values")
-plt.ylabel("module")
-plt.xlabel("channel")
-##plt.legend()
-
-plt.savefig(save_path+"Intsum.png")
-
+#    plt.title("Integrated sum ADC values")
+#    plt.ylabel("module")
+#    plt.xlabel("channel")
+#    plt.legend()
+#
+#    plt.savefig(save_path+"Intsum.png")
+#else:
+#    print("intsum only gets plotted when num_events is 1 for now")
 
 
